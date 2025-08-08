afd81b91597679d09b649276454fb2b61297cef9
LarissaMarianaFontesReis <larissa.reis@rede.ulbra.br>
2025-08-07 21:55:23 -0300

    TDE02 de Backend

---
+++ TDE02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Insira o primeiro número:");
+double numero1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Insira outro número:");
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+double soma = numero1 + numero2;
+double subtracao = numero1 - numero2;
+double multiplicacao = numero1 * numero2;
+double divisao = numero1 / numero2;
+
+Console.WriteLine("Soma:" + soma);
+Console.WriteLine($"Subtração: {subtracao}");
+Console.WriteLine(string.Format("Multiplicação: {0}", multiplicacao));
+Console.WriteLine(string.Format("Divisão: {0}", divisao));
---
+++ Tipos/Program.cs
@@ -0,0 +1,43 @@
+﻿/* int numerointeiro = 10;
+
+double numerodouble = numerointeiro;
+
+double pi = 3.14;
+
+int numint = (int)pi;
+
+string numtexto = "123";
+
+int numero = Convert.ToInt32(numtexto);
+
+//Formatação de strings
+//Concatenação
+
+string nome = "Hirana";
+string mensagem = "Meu nome é " + nome + "!";
+
+//Interpolação
+
+string nomenew = "Niralay";
+int idade = 777;
+string mensagem2 = $"Olá! Meu nome é {nomenew} e tenho {idade} anos!";
+
+//Placeholders
+
+string nomenovo = "ArintheL";
+int idade2 = 20;
+string mensagem3 = string.Format("Olá! Meu nome é {0} e tenho {1} anos!", nomenovo, idade2); */
+
+
+
+using System.Security.Cryptography;
+
+Console.WriteLine("Digite seu nome:");
+
+string? nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua idade:");
+
+int? idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Olá! Meu nome é {nome} e tenho {idade} anos!");
